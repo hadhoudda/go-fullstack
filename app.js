9cf01f11e34c85f9f20f61583967fdacbef31c71
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-06-05 23:36:00 +0200

    install express

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
-const express =require('express')
+const express = require('express');
 
 const app = express();
+app.use(express.json());
+//app.use(express.json()); // permettre d'acceder au coeur de la requette (req.body == coeur de requette)
 
-module.exports= app;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Ces headers permettent :
+
+// d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+
+// d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+
+// d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+app.get('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+// La méthode app.use() vous permet d'attribuer un middleware à une route spécifique de votre application.
+// Le CORS définit comment les serveurs et les navigateurs interagissent,
+//  en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
+
+// Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS),
+//  des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse
+
+// Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS),
+//  des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,61 @@
 const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT ||'3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
 
 //require commande pour importe les package
 //Un serveur Node basique est démarré avec la méthode  createServer
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
-});
+// const server = http.createServer((req, res) => {
+//     res.end('Voilà la réponse du serveur !');
+// });
 
 //createServer c'et un methode prend comme un attribut une fonction qui sera applique à chaque  appele reçu par la serveur
 
-server.listen(process.env.PORT || 3000);
+//server.listen(process.env.PORT || 3000);
 
 // listen pour ecout le serveur et on mettre le port que nous l'ecoute
 //  la méthode end de la réponse pour renvoyer une réponse de type string à l'appelant.
