601bfc11a52dd9bdb54eea7f554e387b01358f24
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-06-06 12:36:04 +0200

    install mongoss

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
 const app = express();
-app.use(express.json());
-//app.use(express.json()); // permettre d'acceder au coeur de la requette (req.body == coeur de requette)
+//import mongoss
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
+app.use(express.json());// permettre d'acceder au coeur de la requette (req.body == coeur de requette)
+
+
+mongoose.connect('mongodb+srv://sajed:sajed2009@cluster0.husfnnb.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//////////////
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -12,49 +23,59 @@ app.use((req, res, next) => {
 });
 
 // Ces headers permettent :
-
 // d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
-
 // d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
-
 // d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
 
+
+//app.use(bodyParser.json()) ancienne ecriture pour accede au coeur de requette
+
+////////// ajoute une objet////////////////////
+
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
   });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+////////////modifier un objet //////////////////
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
+//////////supprime une objet /////////
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+/////recuper une seul objet id///////////////
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({_id: req.params.id})
+  .then(things => res.status(200).json(things))
+      .catch(error => res.status(404).json({ error }));
+})
+///////recuper tout les objets////////////////
 app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));  
 });
 
 // La méthode app.use() vous permet d'attribuer un middleware à une route spécifique de votre application.
 // Le CORS définit comment les serveurs et les navigateurs interagissent,
 //  en spécifiant quelles ressources peuvent être demandées de manière légitime – par défaut, les requêtes AJAX sont interdites.
-
 // Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS),
 //  des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse
-
 // Pour permettre des requêtes cross-origin (et empêcher des erreurs CORS),
 //  des headers spécifiques de contrôle d'accès doivent être précisés pour tous vos objets de réponse
 
